2ae7382ef59f86da73959c2adb7d25bce5c1a4af
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-05-03 01:48:23 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,68 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+galleryContainer.addEventListener('click',handleGalleryContainer)
+
+function createGalleryItems(elements) {
+    return elements.map(({ preview, original, description }) => {
+       return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-src="${original}" 
+      alt=${description}
+    />
+  </a>
+</div>`
+    }).join("")
+};
+
+const instance = basicLightbox.create(`
+    <img class="gallery__modal__img" src=''>
+`,
+    {
+        onShow: instance => {
+            window.addEventListener('keydown', onEscapeClick);
+        },
+        onClose: instance => {
+            window.removeEventListener('keydown', onEscapeClick);
+        },
+    },
+);
+function onEscapeClick(event){
+    if (event.key === 'Escape') {
+        instance.close();
+}}
+
+function handleGalleryContainer(event) {
+    event.preventDefault();
+       
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    instance.element().querySelector('.gallery__modal__img').src = event.target.dataset.src;
+    instance.show();
+
+ 
+}
+
+ 
+
+
+    
+  
+
+
+
+
+
+
+
+
+
